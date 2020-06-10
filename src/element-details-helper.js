7f1a7fd5428808fe4d60fd6c858e2238d3f742d0
Lorand-Edmond Iszlai <lorand.iszlai@algotech.solutions>
2020-06-10 17:47:02 +0300

    Solve export issues for element details helper.

--- src/element-details-helper.js
+++ src/element-details-helper.js
@@ -1,130 +1,133 @@
-var getAllAttributes = function (element) {
-  var attributeDetails = {};
+var attachElementDetailsToGlobal = (returnConstructElementDetails = false) => {
+  var getAllAttributes = function (element) {
+    var attributeDetails = {};
 
-  if (!element.attributes) {
-    return {};
-  }
+    if (!element.attributes) {
+      return {};
+    }
 
-  var attributes = Array.prototype.slice.call(element.attributes);
-  attributes.forEach(function (attribute) {
-    attributeDetails[attribute.nodeName] = attribute.nodeValue;
-  });
+    var attributes = Array.prototype.slice.call(element.attributes);
+    attributes.forEach(function (attribute) {
+      attributeDetails[attribute.nodeName] = attribute.nodeValue;
+    });
 
-  return attributeDetails;
-};
+    return attributeDetails;
+  };
 
-var constructParentsArray = function (element) {
-  var parentArray = [];
-  var index = 0;
+  var constructParentsArray = function (element) {
+    var parentArray = [];
+    var index = 0;
 
-  if (!element.parentElement) {
-    return parentArray;
-  }
-
-  element = element.parentElement;
-
-  while (element !== null && index < 10) {
-    index += 1;
-    parentArray.push(getDirectDetails(element));
+    if (!element.parentElement) {
+      return parentArray;
+    }
 
     element = element.parentElement;
-  }
 
-  return parentArray;
-};
+    while (element !== null && index < 10) {
+      index += 1;
+      parentArray.push(getDirectDetails(element));
 
-var constructDirectChildrenArray = function (element) {
-  var childrenArray = [];
+      element = element.parentElement;
+    }
 
-  if (!element.children) {
-    return null;
-  }
-
-  var children = Array.prototype.slice.call(element.children);
-
-  children.forEach(function (child) {
-    childrenArray.push({
-      details: getDirectDetails(child),
-      children: constructDirectChildrenArray(child),
-    });
-  });
-
-  return childrenArray;
-};
-
-var constructElementDetails = function (selector, testLineItemId, requestedByExtension = false) {
-  var domElement = document.querySelector(selector);
-
-  if (!domElement || (!window.trudonGlobals && !requestedByExtension)) {
-    return null;
-  }
-
-  var elementDetails = getDirectDetails(domElement);
-  if (testLineItemId) {
-    elementDetails.testLineItemId = testLineItemId;
-  }
-  elementDetails.selector = selector;
-  elementDetails.nextSibling = domElement.nextElementSibling ? getDirectDetails(domElement.nextElementSibling) : null;
-  elementDetails.previousSibling = domElement.previousElementSibling ? getDirectDetails(domElement.previousElementSibling) : null;
-  elementDetails.parents = constructParentsArray(domElement);
-  elementDetails.children = constructDirectChildrenArray(domElement);
-  if (window.trudonGlobals) {
-    elementDetails.currentElementGrade = window.trudonGlobals.selectorOptionsHelpers.getFinalGradeForElement(domElement, elementDetails);
-  }
-
-  return elementDetails;
-};
-
-var getDirectDetails = function (element, withTextContent = true) {
-  return {
-    tag: element.tagName,
-    textContent: withTextContent && element.textContent ? element.textContent.replace(/[\n\r\s]+/g, '').substring(0, 128) : null, // trim to 128 characters and delete white spaces tabs and new lines
-    numberOfPreviousSiblings: getPreviousSiblingsNumber(element),
-    numberOfAfterSiblings: getNextSiblingsNumber(element),
-    attributes: getAllAttributes(element),
+    return parentArray;
   };
-};
 
-var getNextSiblingsNumber = function (element) {
-  var nextSiblings = 0;
+  var constructDirectChildrenArray = function (element) {
+    var childrenArray = [];
 
-  while (element.nextElementSibling && nextSiblings < 100) {
-    if (element.nodeType === 3) {
-      continue; // text node not included in nextElementSibling
+    if (!element.children) {
+      return null;
     }
-    element = element.nextElementSibling;
 
-    nextSiblings += 1;
-  }
+    var children = Array.prototype.slice.call(element.children);
 
-  return nextSiblings;
-};
+    children.forEach(function (child) {
+      childrenArray.push({
+        details: getDirectDetails(child),
+        children: constructDirectChildrenArray(child),
+      });
+    });
 
-var getPreviousSiblingsNumber = function (element) {
-  var previousSiblings = 0;
+    return childrenArray;
+  };
 
-  while (element.previousElementSibling && previousSiblings < 100) {
-    if (element.nodeType === 3) {
-      continue; // text node not included in previousElementSibling
+  var constructElementDetails = function (selector, testLineItemId, requestedByExtension = false) {
+    var domElement = document.querySelector(selector);
+
+    if (!domElement || (!window.trudonGlobals && !requestedByExtension)) {
+      return null;
     }
-    element = element.previousElementSibling;
 
-    previousSiblings += 1;
+    var elementDetails = getDirectDetails(domElement);
+    if (testLineItemId) {
+      elementDetails.testLineItemId = testLineItemId;
+    }
+    elementDetails.selector = selector;
+    elementDetails.nextSibling = domElement.nextElementSibling ? getDirectDetails(domElement.nextElementSibling) : null;
+    elementDetails.previousSibling = domElement.previousElementSibling ? getDirectDetails(domElement.previousElementSibling) : null;
+    elementDetails.parents = constructParentsArray(domElement);
+    elementDetails.children = constructDirectChildrenArray(domElement);
+    if (window.trudonGlobals) {
+      elementDetails.currentElementGrade = window.trudonGlobals.selectorOptionsHelpers.getFinalGradeForElement(domElement, elementDetails);
+    }
+
+    return elementDetails;
+  };
+
+  var getDirectDetails = function (element, withTextContent = true) {
+    return {
+      tag: element.tagName,
+      textContent: withTextContent && element.textContent ? element.textContent.replace(/[\n\r\s]+/g, '').substring(0, 128) : null, // trim to 128 characters and delete white spaces tabs and new lines
+      numberOfPreviousSiblings: getPreviousSiblingsNumber(element),
+      numberOfAfterSiblings: getNextSiblingsNumber(element),
+      attributes: getAllAttributes(element),
+    };
+  };
+
+  var getNextSiblingsNumber = function (element) {
+    var nextSiblings = 0;
+
+    while (element.nextElementSibling && nextSiblings < 100) {
+      if (element.nodeType === 3) {
+        continue; // text node not included in nextElementSibling
+      }
+      element = element.nextElementSibling;
+
+      nextSiblings += 1;
+    }
+
+    return nextSiblings;
+  };
+
+  var getPreviousSiblingsNumber = function (element) {
+    var previousSiblings = 0;
+
+    while (element.previousElementSibling && previousSiblings < 100) {
+      if (element.nodeType === 3) {
+        continue; // text node not included in previousElementSibling
+      }
+      element = element.previousElementSibling;
+
+      previousSiblings += 1;
+    }
+
+    return previousSiblings;
+  };
+
+  var addToGlobalVariable = function (testLineItemDetails) {
+    var elementDetails = constructElementDetails(testLineItemDetails.selector, testLineItemDetails.testLineItemId);
+    if (window.trudonGlobals) {
+      window.trudonGlobals.elementDetails.push(elementDetails);
+    }
+
+    return elementDetails;
+  };
+
+  if (returnConstructElementDetails) {
+    return constructElementDetails;
   }
-
-  return previousSiblings;
-};
-
-var addToGlobalVariable = function (testLineItemDetails) {
-  var elementDetails = constructElementDetails(testLineItemDetails.selector, testLineItemDetails.testLineItemId);
-  if (window.trudonGlobals) {
-    window.trudonGlobals.elementDetails.push(elementDetails);
-  }
-
-  return elementDetails;
-};
-
-var attachElementDetailsToGlobal = () => {
   if (window.trudonGlobals) {
     return addToGlobalVariable(window.trudonGlobals.element);
   }
@@ -132,5 +135,5 @@ var attachElementDetailsToGlobal = () => {
 
 export default {
   attachElementDetailsToGlobal,
-  constructElementDetails,
+  constructElementDetails: attachElementDetailsToGlobal(true),
 };
