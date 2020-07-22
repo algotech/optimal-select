3be1d1173f16054071dfc6f71411ee2611b0bf16
Iszlai Lorand-Edmond <iszlailorand@users.noreply.github.com>
2020-07-22 09:17:40 +0300

    Add a randomly created alphabetical-numeral name custom check. (#13)

--- src/selector-generation/produce-selector/relevance-helper.js
+++ src/selector-generation/produce-selector/relevance-helper.js
@@ -6,10 +6,21 @@
 * @return {boolean}
 */
 export function isSelectorRandomlyGenerated(selector) {
-  return (
-    selector.match(/.*([0-9]{1}.{0,3}[0-9]{1}){1}.*/) &&
-    !selector.match(/.*\D+(1[0-2]){1}(\D{1}.*)?$/)
-  );
+  var randomNumeral = !!(selector.match(/.*([0-9]{1}.{0,3}[0-9]{1}){1}.*/) &&
+    !selector.match(/.*\D+(1[0-2]){1}(\D{1}.*)?$/));
+
+  // check this options
+  // - minimul 5 consonants in 6 consecutive characters (2c-1v-3c or 3c-1v-2c)
+  // - minimum 4 consecutive consonants
+  var noVowel = '[^aeiouy]';
+  var vowel = '[aeiouy]';
+  var cccVcc = new RegExp(`.*${noVowel}${noVowel}${noVowel}${vowel}${noVowel}${noVowel}.*`, 'i');
+  var ccVccc = new RegExp(`.*${noVowel}${noVowel}${vowel}${noVowel}${noVowel}${noVowel}.*`, 'i');
+  var fourConsecutiveVovels = new RegExp(`.*${vowel}${vowel}${vowel}${vowel}.*`, 'i');
+
+  var randomLiteral = !!selector.match(cccVcc) || !!selector.match(ccVccc) || !!selector.match(fourConsecutiveVovels);
+
+  return randomNumeral || randomLiteral;
 }
 
 export default {
