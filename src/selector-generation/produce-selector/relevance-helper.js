ab6c7a5b2c4502d6d28cba7b13c407c7ee1f3f74
Robert Vintila <robert.vintila@algotech.solutions>
2020-09-18 15:21:14 +0300

    Dont consider - and _ as consonants.

--- src/selector-generation/produce-selector/relevance-helper.js
+++ src/selector-generation/produce-selector/relevance-helper.js
@@ -12,7 +12,7 @@ export function isSelectorRandomlyGenerated(selector) {
   // check this options
   // - minimul 5 consonants in 6 consecutive characters (2c-1v-3c or 3c-1v-2c)
   // - minimum 4 consecutive consonants
-  var noVowel = '[^aeiouy]';
+  var noVowel = '[^aeiouy_-]';
   var vowel = '[aeiouy]';
   var cccVcc = new RegExp(`.*${noVowel}${noVowel}${noVowel}${vowel}${noVowel}${noVowel}.*`, 'i');
   var ccVccc = new RegExp(`.*${noVowel}${noVowel}${vowel}${noVowel}${noVowel}${noVowel}.*`, 'i');
